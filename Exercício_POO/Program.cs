0882218988fc649a60cfe71526a042711b9a70b4
MatheusVDMelo <matheus.vitordemelo@gmail.com>
2026-05-09 22:32:08 -0300

    adicionado classe Produto.cs, validado funcionalidades.

--- Exercício_POO/Program.cs
+++ Exercício_POO/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using exercicioPoo;
+
+Produto Produto01 = new Produto()
+{
+    Nome = "Sabão",
+    Preco = 2
+};
+Produto Produto02 = new Produto()
+{
+    Nome = "Refrigerante",
+    Preco = 5
+};
+
+Produto01.ExibirNomeValorProduto();
+Produto02.ExibirNomeValorProduto();
