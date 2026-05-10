603e76c43c3e04b3855bc94060934151e1b00f84
MatheusVDMelo <matheus.vitordemelo@gmail.com>
2026-05-10 00:14:24 -0300

    adicionado classe Pessoa.cs, validado funcionalidades.

--- Exercício_POO/Program.cs
+++ Exercício_POO/Program.cs
@@ -1,15 +1,24 @@
 ﻿using exercicioPoo;
 
-Produto Produto01 = new Produto()
-{
-    Nome = "Sabão",
-    Preco = 2
-};
-Produto Produto02 = new Produto()
-{
-    Nome = "Refrigerante",
-    Preco = 5
-};
+//Exercício 01
+//
+//Produto Produto01 = new Produto()
+//{
+//    Nome = "Sabão",
+//    Preco = 2
+//};
+//Produto Produto02 = new Produto()
+//{
+//    Nome = "Refrigerante",
+//    Preco = 5
+//};
 
-Produto01.ExibirNomeValorProduto();
-Produto02.ExibirNomeValorProduto();
+//Produto01.ExibirNomeValorProduto();
+//Produto02.ExibirNomeValorProduto();
+
+Pessoa Pessoa01 = new Pessoa()
+{
+    Nome = "Larissa",
+    Idade = 32
+};
+Pessoa01.ExibirNomeIdadePessoa();
