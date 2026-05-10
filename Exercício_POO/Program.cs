807396cc4b6e261bf0c9548aba7b862c50fe0cbc
MatheusVDMelo <matheus.vitordemelo@gmail.com>
2026-05-10 16:52:20 -0300

    adicionado exibicao por forech classe Produto.cs, validado funcionalidades.

--- Exercício_POO/Program.cs
+++ Exercício_POO/Program.cs
@@ -38,39 +38,50 @@
 //Exercício 04
 //
 
-ContaBancaria Usuario01 = new ContaBancaria
-{
-    Saldo = 0
-};
-byte opcao = 0;
-while (opcao != 3)
-{
-    Console.WriteLine("Informe operação deseja:");
-    Console.WriteLine("1 para depósito.");
-    Console.WriteLine("2 para saque.");
-    Console.WriteLine("3 para sair.");
-    opcao = byte.Parse(Console.ReadLine());
-    Console.Clear();
-    if (opcao != 3)
-    {
-        Usuario01.InserirValor();
-        switch (opcao)
-        {
-            case 1:
-                Console.Clear();
-                Usuario01.Depositar();
-                Console.WriteLine($"Seu atual: {Usuario01.Saldo}");
-                break;
-            case 2:
-                Console.Clear();
-                Usuario01.Sacar();
-                break;
-        }
-    }
-    else
-    {
-        Console.WriteLine($"Acesso encerrado com sucesso.");
-        return;
-    }
-}
+//ContaBancaria Usuario01 = new ContaBancaria
+//{
+//    Saldo = 0
+//};
+//byte opcao = 0;
+//while (opcao != 3)
+//{
+//    Console.WriteLine("Informe operação deseja:");
+//    Console.WriteLine("1 para depósito.");
+//    Console.WriteLine("2 para saque.");
+//    Console.WriteLine("3 para sair.");
+//    opcao = byte.Parse(Console.ReadLine());
+//    Console.Clear();
+//    if (opcao != 3)
+//    {
+//        Usuario01.InserirValor();
+//        switch (opcao)
+//        {
+//            case 1:
+//                Console.Clear();
+//                Usuario01.Depositar();
+//                Console.WriteLine($"Seu atual: {Usuario01.Saldo}");
+//                break;
+//            case 2:
+//                Console.Clear();
+//                Usuario01.Sacar();
+//                break;
+//        }
+//    }
+//    else
+//    {
+//        Console.WriteLine($"Acesso encerrado com sucesso.");
+//        return;
+//    }
+//}
 
+//Exercício 05
+//
+Produto produto01 = new Produto() { Nome = "Detergente", Preco = 8 };
+Produto produto02 = new Produto() { Nome = "Água sanitária", Preco = 9 };
+Produto produto03 = new Produto() { Nome = "Sabão em pó", Preco = 18 };
+Produto[] Lista = [produto01, produto02, produto03];
+foreach (Produto Indice in Lista)
+{
+    Console.WriteLine($"Produto:{Indice.Nome}, Preco:{Indice.Preco}");
+}
+;
